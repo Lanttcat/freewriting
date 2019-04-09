96cbb6b92f7d71341ba226029d4eaa23f8467df4
NikoManiac <niko.sky.lan@gmail.com>
2019-04-09 22:34:06 +0800

    test for ci

---
+++ .circleci/deploy-master.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+user=$REMOTE_NAME
+host=$REMOTE_HOST
+src=`pwd`"/"
+#des=/data/www/api.www.zjj7.com/
+now=`date +"%Y-%m-%d %H:%M:%S"`
+
+#rsync -vzrc --delete  --exclude ".git"  --exclude ".env"   --exclude ".circleci"   $src  $user@$host:$des
+
+#ssh $user@$host "sudo chown -R www:www $des"
+
+#ssh $user@$host "chmod -R 775 $des/bootstrap/cache && chmod -R 775 $des/storage && cd $des && pwd && ls -al"
+echo $user
+echo $host
+echo "$now update $host $des code"
