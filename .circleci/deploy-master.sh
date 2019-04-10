f575504c3cd1e2b9ce302b33a9694af047832833
NikoManiac <niko.sky.lan@gmail.com>
2019-04-10 12:53:29 +0800

    test for ci : adjust work space 8 and publish 7

--- .circleci/deploy-master.sh
+++ .circleci/deploy-master.sh
@@ -6,9 +6,4 @@ src=`pwd`"/build/"
 des=~/test/
 now=`date +"%Y-%m-%d %H:%M:%S"`
 
-rsync -vzrc --delete  --exclude ".git"  --exclude ".env"   --exclude ".circleci"   $src  $user@$host:$des
-#ssh $user@$host "sudo chown -R www:www $des"
-#ssh $user@$host "chmod -R 775 $des/bootstrap/cache && chmod -R 775 $des/storage && cd $des && pwd && ls -al"
-echo $user
-echo $host
-echo "$now update $host $des code"
+rsync -vzrc $src  $user@$host:$des
